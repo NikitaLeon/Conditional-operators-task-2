9cf43207e1e84fd412d2392ab6e78470f8c50eee
Nikita Leonov <kontrofakt13@gmail.com>
2024-08-15 13:53:03 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 100;
+        int replenishment = 1100;
+        int bonus;
+        if (replenishment > 1000) {
+            bonus = replenishment / 100;
+        }
+        else {
+            bonus = 0;
+        }
+        System.out.println("Итоговый бонус " + bonus);
+
+    }
+}
